b5d89436f74f9a60637fa306213d2cdc835d55fa
RivnatZille <lucasvzille@hotmail.com>
2022-09-27 17:12:49 -0300

    Finalizado até a aula 25

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,52 @@
-<script setup>
+<script>
 import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import { ref, reactive } from "vue";
+
+export default {
+  name: "App",
+
+  components: { HelloWorld },
+
+  setup() {
+    const user = reactive({
+      first_name: 'Jon',
+      last_name: 'Snow'
+    });
+
+    const admin = ref({
+      first_name: 'Admin',
+      last_name: 'Master'
+    });
+
+    let name = 'Lucas';
+
+    const changeName = () => {
+      name = 'Jon Sow';
+      user.first_name = 'Sansa';
+      admin.value.first_name = 'Ademir';
+    };
+
+    return {
+      name,
+      admin,
+      user,
+      changeName
+    }
+  }
+}
 </script>
 
 <template>
   <header>
+    {{ name }}
+    <br><br>
+    <h5>User</h5>
+    {{ user.first_name }} {{ user.last_name }}
+    <br><br>
+    <h5>Admin</h5>
+    {{ admin.first_name }} {{ admin.last_name }}
     <img
+      @click = "changeName()"
       alt="Vue logo"
       class="logo"
       src="./assets/logo.svg"
@@ -34,20 +75,20 @@ header {
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+  /*header {*/
+  /*  display: flex;*/
+  /*  place-items: center;*/
+  /*  padding-right: calc(var(--section-gap) / 2);*/
+  /*}*/
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  /*.logo {*/
+  /*  margin: 0 2rem 0 0;*/
+  /*}*/
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  /*header .wrapper {*/
+  /*  display: flex;*/
+  /*  place-items: flex-start;*/
+  /*  flex-wrap: wrap;*/
+  /*}*/
 }
 </style>
