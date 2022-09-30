5551ddebc0bd93a7230f5e8e3161be2fef9037b3
RivnatZille <lucasvzille@hotmail.com>
2022-09-30 14:48:43 -0300

    Escopo final do projeto feito.

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
       <template v-else>
         <TodoFormAdd />
 
-        <TodoItems />
+        <TodoItems v-if="$store.state.todos.length"/>
 
-        <TodoEmpty />
+        <TodoEmpty v-else/>
       </template>
     </div>
   </div>
--- src/store/index.js
+++ src/store/index.js
@@ -2,45 +2,74 @@ import { createStore } from "vuex";
 import axios from "axios";
 
 export default createStore({
-    state: {
-        todos: []
+  state: {
+    todos: [],
+  },
+
+  mutations: {
+    storeTodos(state, payload) {
+      state.todos = payload;
     },
 
-    mutations: {
-        storeTodos(state, payload) {
-            state.todos = payload;
-        },
+    storeTodo(state, payload) {
+      const index = state.todos.findIndex(todo => todo.id === payload.id);
 
-        storeTodo(state, payload) {
-            state.todos.push(payload);
-        }
+      if (index >= 0) {
+        state.todos.splice(index, 1, payload);
+      } else {
+        state.todos.push(payload);
+      }
     },
 
-    actions: {
-        getTodos({ commit }) {
-            return new Promise((resolve) => {
-                setTimeout(() => {
-                    return axios.get('http://localhost:3000/todos')
-                        .then((response) => {
-                            commit("storeTodos", response.data);
-                            resolve();
-                        });
-                }, 1000);
-            });
-        },
+    deleteTodo(state, id) {
+      const index = state.todos.findIndex(todo => todo.id === id);
 
-        addTodo({ commit }, data) {
-            return axios.post('http://localhost:3000/todos', data).then((response) => {
-                commit('storeTodo', response.data);
-            });
-        }
-    },
-
-    getters: {
-
-    },
-
-    modules: {
-
+      if (index >= 0) {
+        state.todos.splice(index, 1);
+      }
     }
+  },
+
+  actions: {
+    getTodos({ commit }) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          return axios
+              .get("http://localhost:3000/todos")
+              .then((response) => {
+            commit("storeTodos", response.data);
+            resolve();
+          });
+        }, 1000);
+      });
+    },
+
+    addTodo({ commit }, data) {
+      return axios
+        .post("http://localhost:3000/todos", data)
+        .then((response) => {
+          commit("storeTodo", response.data);
+        });
+    },
+
+    updateTodo({ commit }, { id, data }) {
+      return axios
+        .put(`http://localhost:3000/todos/${id}`, data)
+        .then((response) => {
+          commit('storeTodo', response.data);
+        });
+    },
+
+    deleteTodo({ commit }, id) {
+      return axios
+        .delete(`http://localhost:3000/todos/${id}`)
+        .then(() => {
+          commit('deleteTodo', id);
+        });
+    },
+  },
+
+  getters: {},
+
+  modules: {},
 });
